63567ad8ce05897f4a3ac45165f691daba6023de
Felipe Kawassaki <fekawassaki@gmail.com>
2020-03-18 09:39:10 -0300

    Updates

---
+++ precache-manifest.4908495725a05afa508ab2bf003a4d4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031f9e77f520076ef19bfcf876d6f466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52967ee40e13cab0e1b3",
+    "url": "/static/js/2.2ecababe.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.2ecababe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1baa32f02c34fe2486c3",
+    "url": "/static/js/main.5372a866.chunk.js"
+  },
+  {
+    "revision": "40f2ce8183704b84d747",
+    "url": "/static/js/runtime-main.9e1e62b6.js"
+  }
+]);
